c990ed052adbc5d21ee1c7b03d66305ca0c9ade0
Nick Clifton <nicholas.clifton@gmail.com>
2017-09-11 23:29:31 -0400

    updates to marker object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { Http }       from '@angular/http';
-import { HttpService } from './http.service';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { Marker } from './marker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
-    
-    private title: string = 'My first AGM project';
-    private lat: number = 51.678418;
-    private lng: number = 7.809007;
-    private dataUrls = ['F01705150050.json','F01705150090.json'];
-    private dataUrl = 'F01705150050.json';
-    geoEvent: any;
-    results: string[];
-    constructor(private http: Http) {}
-    ngOnInit(): void {
-        // Make the HTTP request:
-        this.http.get(this.dataUrl).subscribe(data => {
-          // Read the result field from the JSON response.
-          this.results = data['results'];
-        });
-      }
-    /*constructor(private _httpService: HttpService) {
-        this._httpService.getJSON(this.dataUrl)
-        .subscribe(
-        (res) => {
-            this.geoEvent = res;
-        },
-        (error) => console.log("error : " + error),
-        () => console.log('Error in GetApplication in Login : ' + Error)
-    );
-    }*/
-}
 
+
+
+export class AppComponent{
+       
+    private title: string = 'Prominent Edge Interview - Nick Clifton';
+    private lat: number = 37.541885;
+    private lng: number = -77.440624;
+    private dataUrls = ['assets/F01705150050.json','assets/F01705150090.json'];
+    private dataUrl = 'assets/F01705150050.json';
+    markers: Marker[];
+    results: string[];
+    constructor(private http: HttpClient) {}
+    
+    ngOnInit(): void {
+        for(let dataUrl of this.dataUrls){
+            this.http.get(dataUrl).subscribe(data => {
+                var marker = new Marker();
+                marker.common_place_name=data["address"]["common_place_name"];
+                marker.address_line1=data["address"]["address_line1"];
+                marker.latitude=data["address"]["latitude"];
+                marker.longitude=data["address"]["longitude"];
+                marker.response_zone=data["address"]["longitude"];
+                marker.comments=data["description"]["comments"];
+                marker.event_opened=new Date(data["description"]["event_opened"]);
+                marker.event_closed=new Date(data["description"]["event_closed"]);
+                marker.getWeatherData();
+                marker.getParcelData();
+            });
+        }
+    }
+}
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AgmCoreModule } from '@agm/core';
 
@@ -10,6 +10,7 @@ import { AgmCoreModule } from '@agm/core';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AgmCoreModule.forRoot({
         apiKey: 'AIzaSyCAhr8F9u8ihlShdn9Hz03Jlqj9Ly2kkLs'
       })
